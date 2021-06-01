edcf010b6c76d49fbae541406cacc279c0582a60
Eugene Gusarov <2000admiral@mail.ru>
2021-06-02 00:46:54 +0300

    project created

---
+++ src/main/java/com/test/telda/TeldaApplication.java
@@ -0,0 +1,13 @@
+package com.test.telda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeldaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TeldaApplication.class, args);
+    }
+
+}
